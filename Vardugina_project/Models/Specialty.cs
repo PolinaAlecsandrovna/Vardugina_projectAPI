9d468514cb47c1aedf9bd1f70e7ee0df97408ce4
Polina <poll.vard.123@gmail.com>
2026-03-17 18:44:11 +0700

    Creating Specialty.cs, StudentGroup.cs, Teacher.cs, Subject.cs, Weekday.cs, and Schedule.cs files

--- Vardugina_project/Models/Specialty.cs
+++ Vardugina_project/Models/Specialty.cs
@@ -1,6 +1,17 @@
-﻿namespace Vardugina_project.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Vardugina_project.Models
 {
+    [Table("specialties")]
     public class Specialty
     {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; } = null!;
     }
 }

94b5b595e43fd5a2de66328cd07350d0693f8165
Polina <poll.vard.123@gmail.com>
2026-03-17 21:10:54 +0700

    Creating an API Controller

---
+++ Vardugina_project/Controllers/ScheduleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vardugina_project.Data;
+using Vardugina_project.Services;
+using Vardugina_project.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Vardugina_project.Controllers
+{
+    [ApiController]
+    [Route("api/schedule")]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IScheduleService _service;
+        public ScheduleController(IScheduleService service, AppDbContext db)
+        {
+            _service = service;
+        }
+        [HttpGet("group/{groupName}")]
+        public async Task<IActionResult> GetSchedule(string groupName, DateTime start, DateTime end)
+        {
+            var result = await _service.GetScheduleForGroup(groupName, start, end);
+            return Ok(result);
+        }
+
+    }
+}

f6e44c791362bf34f9b51b772379c71eb8b090c6
Polina <poll.vard.123@gmail.com>
2026-03-17 20:59:04 +0700

    IScheduleService interface, ScheduleService implementation, and service registration

---
+++ Vardugina_project/Services/IScheduleService.cs
@@ -0,0 +1,9 @@
+﻿using Vardugina_project.DTO;
+
+namespace Vardugina_project.Services
+{
+    public interface IScheduleService
+    {
+        Task<List<ScheduleByDateDto>> GetScheduleForGroup(string groupName, DateTime startDate, DateTime endDate);
+    }
+}

3c68a5423f33ee65f4a94a6ea33ddc9b411f6eea
Polina <poll.vard.123@gmail.com>
2026-03-17 20:10:27 +0700

    Implementation of secure storage of settings and database connection

--- Vardugina_project/Program.cs
+++ Vardugina_project/Program.cs
@@ -1,6 +1,20 @@
+using Vardugina_project.Data;
+using Vardugina_project.Middlewares;
+using Vardugina_project.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+DotNetEnv.Env.Load();
+var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+ $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+ $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+ $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+
+$"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+builder.Services.AddDbContext<AppDbContext>(options =>
+ options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,6 +33,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseRouting();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 
 app.MapControllers();
 

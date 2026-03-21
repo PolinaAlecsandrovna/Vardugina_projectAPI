1d1fa3835ef38b1d7bd77e0385ba287c84a157b3
Polina <poll.vard.123@gmail.com>
2026-03-22 00:00:12 +0700

    Creating the GroupsController api controller

---
+++ Vardugina_project/Controllers/GroupsController.cs
@@ -0,0 +1,32 @@
+﻿using Vardugina_project.DTO;
+using Vardugina_project.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vardugina_project.Controllers
+{
+    [ApiController]
+    [Route("api/group")]
+    public class GroupsController : Controller
+    {
+        private readonly IScheduleService _service;
+
+        public GroupsController(IScheduleService service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+        [ProducesResponseType(typeof(List<GroupDto>), 200)]
+        public async Task<ActionResult<List<GroupDto>>> GetAlllGroups()
+        {
+            try
+            {
+                var groups = await _service.GetAllGroupsAsync();
+                return Ok(groups);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { error = "Не удалось загрузить список групп" });
+            }
+        }
+    }
+}

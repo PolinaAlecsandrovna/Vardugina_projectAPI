6345b3ba4f1c87bf785b8a89b29089e0a7c5dd1f
Polina <poll.vard.123@gmail.com>
2026-03-21 22:47:21 +0700

    Add method GetAllGroupsAsync

--- Vardugina_project/Services/IScheduleService.cs
+++ Vardugina_project/Services/IScheduleService.cs
@@ -5,5 +5,6 @@ namespace Vardugina_project.Services
     public interface IScheduleService
     {
         Task<List<ScheduleByDateDto>> GetScheduleForGroup(string groupName, DateTime startDate, DateTime endDate);
+        Task<List<GroupDto>> GetAllGroupsAsync();
     }
 }

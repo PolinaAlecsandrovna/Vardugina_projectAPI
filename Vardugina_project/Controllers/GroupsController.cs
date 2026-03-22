62d897e6a48cf8024939b28b9233467768d92589
Polina <poll.vard.123@gmail.com>
2026-03-22 19:44:13 +0700

    adding a search by group name

--- Vardugina_project/Controllers/GroupsController.cs
+++ Vardugina_project/Controllers/GroupsController.cs
@@ -2,6 +2,7 @@
 using Vardugina_project.Services;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace Vardugina_project.Controllers
 {
     [ApiController]
@@ -14,19 +15,29 @@ namespace Vardugina_project.Controllers
         {
             _service = service;
         }
+
         [HttpGet]
         [ProducesResponseType(typeof(List<GroupDto>), 200)]
-        public async Task<ActionResult<List<GroupDto>>> GetAlllGroups()
+        public async Task<ActionResult<List<GroupDto>>> GetAllGroups([FromQuery] string? search = null)
         {
             try
             {
                 var groups = await _service.GetAllGroupsAsync();
+
+                if (!string.IsNullOrWhiteSpace(search))
+                {
+                    groups = groups
+                        .Where(g => g.GroupName.Contains(search, StringComparison.OrdinalIgnoreCase))
+                        .ToList();
+                }
+
                 return Ok(groups);
             }
             catch (Exception ex)
             {
-                return StatusCode(500, new { error = "Не удалось загрузить список групп" });
+                return StatusCode(500, new { error = "Не удалось загрузить список групп.", details = ex.Message });
             }
         }
+
     }
 }
